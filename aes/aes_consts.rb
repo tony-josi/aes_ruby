738a87c387f755a4d154233f82f7fe85a88ad923
Tony Josi <tonyjosinew@gmail.com>
2022-07-27 12:51:22 +0530

    test file moved

--- aes/aes_consts.rb
+++ aes/aes_consts.rb
@@ -1,6 +1,6 @@
 
 # /* AES Word size */
-AES_WORD_SIZE   					= (4)
+AES_WORD_SIZE						= (4)
 
 # /* 12.8 KB per data segment. */
 AES_DATA_SIZE_PER_SEGMENT			= (12800)
@@ -8,9 +8,9 @@ FILE_IO_CHUNK_SIZE_BYTES			= (12800000 * 2)
 MAX_ALGO_WORKER_THREAD_COUNT		= (50)
 
 # /* Metdata size should be (AES_WORD_SIZE * AES_WORD_SIZE) */ 
-AES_META_DATA_SIZE				    = (AES_WORD_SIZE * AES_WORD_SIZE)  
-AES_META_DATA_PADD_SIZE_OFFSET	    = (0) 
-AES_META_DATA_CHECK_SUM_OFFSET	    = (8) 
+AES_META_DATA_SIZE					= (AES_WORD_SIZE * AES_WORD_SIZE)  
+AES_META_DATA_PADD_SIZE_OFFSET		= (0) 
+AES_META_DATA_CHECK_SUM_OFFSET		= (8) 
 
 # /* Maximum supported plain text key size. */
 MAX_SUPPORTED_PLAIN_KEY_SIZE		= (32)
